76cebcacda0e78f47ddd883cb5c92520b8a4724c
Nathan Hemingway <nhemingway@gmail.com>
2022-10-28 17:26:16 -0700

    remove unused request data

--- pkg/req/requests.go
+++ pkg/req/requests.go
@@ -1,3 +1,4 @@
+// Package req contains the collector requests
 package req
 
 import (
@@ -8,7 +9,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-//go:embed reqs.yaml
+//go:embed reqs.json
 var reqsData []byte
 
 // Mod modifies an aci Request
@@ -16,30 +17,12 @@ type Mod = func(*aci.Req)
 
 // Request is an HTTP request.
 type Request struct {
-	Class  string            // MO class
-	Prefix string            // Name for filename and class in DB
-	Query  map[string]string // Query parameters
-	Path   string
-}
-
-// Normalize chooses correct class and path.
-// This should be run on all manually created Requests structs.
-func (req *Request) Normalize() *Request {
-	if req.Prefix == "" {
-		req.Prefix = req.Class
-	}
-	req.Path = "/api/class/" + req.Class
-	return req
+	Class string            // MO class
+	Query map[string]string // Query parameters
 }
 
 // GetRequests returns normalized requests
 func GetRequests() (reqs []Request, err error) {
 	err = yaml.Unmarshal(reqsData, &reqs)
-	if err != nil {
-		return
-	}
-	for i := 0; i < len(reqs); i++ {
-		reqs[i].Normalize()
-	}
 	return
 }
